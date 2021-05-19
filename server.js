96add297cceb09c1833537c25f181d06a55af541
Samyak2107 <66407436+Samyak2107@users.noreply.github.com>
2021-05-19 16:41:56 +0530

    updating server

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.put('/image', (req, res) => {image.handleImage(req, res, database)})
 
 app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, database)});
 
-app.listen(3001, ()=> {
-    console.log("App is running on port 3001.");
+app.listen(process.env.PORT || 3001, ()=> {
+    console.log(`App is running on port ${process.env.PORT}`);
 })
